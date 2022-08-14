13730fa428b360fa25d9b846f305f60e3239262b
reinforchu <rein@hack.vet>
2022-08-14 17:39:44 +0900

    First commit
    Create repository

---
+++ CyrillicHomographGen/CyrillicHomographGen/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace CyrillicHomographGen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openBrowserToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.sampleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(126, 33);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(264, 19);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(126, 58);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(264, 19);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(396, 31);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(496, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openBrowserToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openBrowserToolStripMenuItem
+            // 
+            this.openBrowserToolStripMenuItem.Name = "openBrowserToolStripMenuItem";
+            this.openBrowserToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openBrowserToolStripMenuItem.Text = "Open";
+            this.openBrowserToolStripMenuItem.Click += new System.EventHandler(this.openBrowserToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(148, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sampleToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "ASCII Host name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Cyrillic Homograph";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(396, 56);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 86);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 12);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Open Firefox URL";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(396, 81);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Attack";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(126, 84);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(264, 19);
+            this.textBox3.TabIndex = 10;
+            // 
+            // sampleToolStripMenuItem
+            // 
+            this.sampleToolStripMenuItem.Name = "sampleToolStripMenuItem";
+            this.sampleToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.sampleToolStripMenuItem.Text = "Sample";
+            this.sampleToolStripMenuItem.Click += new System.EventHandler(this.sampleToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(496, 112);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Cyrillic Homograph Generator";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openBrowserToolStripMenuItem;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ToolStripMenuItem sampleToolStripMenuItem;
+    }
+}
+
---
+++ CyrillicHomographGen/CyrillicHomographGen/Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace CyrillicHomographGen
+{
+    public partial class Form1 : Form
+    {
+        StringBuilder domain = new StringBuilder();
+        public Form1()
+        {
+            InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            DialogResult eula = MessageBox.Show("This software is intended for cybersecurity research purposes.\nTherefore you are not allowed to exploit and abuse the results of this tool in any way. And the developer does not take any responsibility.\nDo you still agree and continue?", "End User License Agreement", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+            if (eula == DialogResult.No)
+            {
+                Environment.Exit(0);
+            }
+            if (!System.IO.File.Exists(@"C:\Program Files\Mozilla Firefox\firefox.exe"))
+            {
+                MessageBox.Show("It looks like you don't have Firefox installed.", "Firefox 404 Not found", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string inputString = textBox1.Text;
+            foreach (char character in inputString)
+            {
+                if (Regex.IsMatch(character.ToString(), "a")) {
+                    domain.Append("а");
+                }
+                if (Regex.IsMatch(character.ToString(), "e"))
+                {
+                    domain.Append("е");
+                }
+                if (Regex.IsMatch(character.ToString(), "o"))
+                {
+                    domain.Append("о");
+                }
+                if (Regex.IsMatch(character.ToString(), "r"))
+                {
+                    domain.Append("г");
+                }
+                if (Regex.IsMatch(character.ToString(), "p"))
+                {
+                    domain.Append("р");
+                }
+                if (Regex.IsMatch(character.ToString(), "k"))
+                {
+                    domain.Append("к");
+                }
+                if (Regex.IsMatch(character.ToString(), "s"))
+                {
+                    domain.Append("ѕ");
+                }
+                if (Regex.IsMatch(character.ToString(), "c"))
+                {
+                    domain.Append("с");
+                }
+                if (Regex.IsMatch(character.ToString(), "x"))
+                {
+                    domain.Append("х");
+                }
+                if (Regex.IsMatch(character.ToString(), "y"))
+                {
+                    domain.Append("у");
+                }
+                if (Regex.IsMatch(character.ToString(), "i"))
+                {
+                    domain.Append("і");
+                }
+                if (Regex.IsMatch(character.ToString(), "l"))
+                {
+                    domain.Append("ӏ");
+                }
+                if (Regex.IsMatch(character.ToString(), "3"))
+                {
+                    domain.Append("ӡ");
+                }
+                if (Regex.IsMatch(character.ToString(), "h"))
+                {
+                    domain.Append("һ");
+                }
+                if (Regex.IsMatch(character.ToString(), "b"))
+                {
+                    domain.Append("Ь");
+                }
+                if (Regex.IsMatch(character.ToString(), "m"))
+                {
+                    domain.Append("м");
+                }
+                if (Regex.IsMatch(character.ToString(), "q"))
+                {
+                    domain.Append("ԛ");
+                }
+                if (Regex.IsMatch(character.ToString(), "w"))
+                {
+                    domain.Append("ԝ");
+                }
+                if (Regex.IsMatch(character.ToString(), "j"))
+                {
+                    domain.Append("ј");
+                }
+                if (Regex.IsMatch(character.ToString(), "f"))
+                {
+                    domain.Append("ғ");
+                }
+                if (Regex.IsMatch(character.ToString(), "t"))
+                {
+                    domain.Append("т");
+                }
+                if (Regex.IsMatch(character.ToString(), "u"))
+                {
+                    domain.Append("ч");
+                }
+                if (!Regex.IsMatch(character.ToString(), "[aeorpkscxyil3bmqwjftu]"))
+                {
+                    MessageBox.Show("There was no alternate letter for '" + character + "'.", "Exception", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                    domain.Clear();
+                    break;
+                }
+            }
+            textBox2.Text = domain.ToString();
+            textBox3.Text = "http://" + domain.ToString() + ".com/";
+            domain.Clear();
+            button1.Enabled = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            button1.Enabled = true;
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Cyrillic Homograph Generator\nVersion 1.0.0.0\nCopyright (C) reinforchu", "Cyrillic Homograph Generator", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void openBrowserToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (textBox2.TextLength > 0)
+            {
+                Process.Start(textBox3.Text);
+            } else
+            {
+                MessageBox.Show("Domain name not set.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (textBox2.TextLength > 0)
+            {
+                Process.Start(@"C:\Program Files\Mozilla Firefox\firefox.exe", textBox3.Text);
+            }
+            else
+            {
+                MessageBox.Show("URL not set.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void sampleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "opera";
+            button1.PerformClick();
+        }
+    }
+}
