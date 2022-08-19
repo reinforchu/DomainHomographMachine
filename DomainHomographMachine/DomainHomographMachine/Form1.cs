de4e1d8da480c4fbff80b0672c4e7d587c14cddf
reinforchu <rein@hack.vet>
2022-08-19 11:49:11 +0900

    Refactoring
    Optimizing redundant code.

--- DomainHomographMachine/DomainHomographMachine/Form1.cs
+++ DomainHomographMachine/DomainHomographMachine/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Text;
@@ -10,6 +11,13 @@ namespace CyrillicHomographGen
     public partial class Form1 : Form
     {
         StringBuilder domain = new StringBuilder();
+        Dictionary<string, string> CyrillicTable = new Dictionary<string, string>() { { "a", "а" }, { "e", "е" }, { "o", "о" }, { "r", "г" }, { "p", "р" }, { "k", "к" }, { "s", "ѕ" }, { "c", "с" }, { "x", "х" }, { "y", "у" }, { "i", "і" }, { "l", "ӏ" }, { "h", "һ" }, { "b", "Ь" }, { "m", "м" }, { "q", "ԛ" }, { "w", "ԝ" }, { "j", "ј" }, { "f", "ғ" }, { "t", "т" }, { "u", "ч" } };
+        const string CyrillicRegex = "[aeorpkscxyilbmqwjftu0-9-]";
+        Dictionary<string, string> GreekAndCopticTable = new Dictionary<string, string>() { { "a", "α" }, { "y", "γ" }, { "l", "ι" }, { "k", "κ" }, { "v", "ν" }, { "i", "ί" }, { "o", "ο" }, { "p", "ρ" }, { "u", "υ" }, { "x", "χ" } };
+        const string GreekAndCopticRegex = "[aeorpkscxyilbmqwjftu0-9-]";
+        Dictionary<string, string> ArmenianTable = new Dictionary<string, string>() { { "t", "ե" }, { "q", "զ" }, { "d", "ժ" }, { "l", "լ" }, { "h", "հ" }, { "j", "յ" }, { "n", "ո" }, { "u", "ս" }, { "g", "ց" }, { "o", "օ" } };
+        const string ArmenianRegex = "[tqdlhjnugo0-9-]";
+
         public Form1()
         {
             InitializeComponent();
@@ -55,41 +63,6 @@ namespace CyrillicHomographGen
             MessageBox.Show("This software is intended for cybersecurity research purposes.\nTherefore you are not allowed to exploit and abuse the results of this tool in any way. And the developer does not take any responsibility.", "End User License Agreement", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
 
-        private void button4_Click(object sender, EventArgs e) // Cyrillic
-        {
-            var CyrillicTable = new Dictionary<string, string>()
-            {
-                {"a", "а"}, {"e", "е"}, {"o", "о"}, {"r", "г"}, {"p", "р"}, {"k", "к"}, {"s", "ѕ"}, {"c", "с"}, {"x", "х"}, {"y", "у"}, {"i", "і"}, {"l", "ӏ"}, {"h", "һ"}, {"b", "Ь"}, {"m", "м"}, {"q", "ԛ"}, {"w", "ԝ"}, {"j", "ј"}, {"f", "ғ"}, {"t", "т"}, {"u", "ч"}
-            };
-            foreach (char character in textBox1.Text)
-            {
-                if (!Regex.IsMatch(character.ToString(), "[aeorpkscxyilbmqwjftu0-9-]"))
-                {
-                    MessageBox.Show("There was no substituted letter for '" + character + "'.\nStop searching for the Cyrillic subcategory.", "Exception - Cyrillic", MessageBoxButtons.OK, MessageBoxIcon.Stop);
-                    domain.Clear();
-                    break;
-                }
-                foreach (KeyValuePair<string, string> cyrillic in CyrillicTable)
-                {
-                    if (Regex.IsMatch(character.ToString(), cyrillic.Key))
-                    {
-                        domain.Append(cyrillic.Value);
-                        break;
-                    }
-                }
-                if (Regex.IsMatch(character.ToString(), "[0-9-]"))
-                {
-                    domain.Append(character);
-                }
-            }
-            textBox2.Text = domain.ToString();
-            if (!string.IsNullOrEmpty(domain.ToString()))
-            {
-                textBox3.Text = "http://" + domain.ToString() + ".com/";
-            }
-            domain.Clear();
-        }
-
         private void button1_Click(object sender, EventArgs e)
         {
             textBox1.Clear();
@@ -99,60 +72,38 @@ namespace CyrillicHomographGen
             textBox5.Clear();
         }
 
-        private void button2_Click(object sender, EventArgs e)  // Greek and Coptic
+        private void button4_Click(object sender, EventArgs e)
         {
-            var GreekAndCopticTable = new Dictionary<string, string>()
-            {
-                {"a", "α"}, {"y", "γ"}, {"l", "ι"}, {"k", "κ"}, {"v", "ν"}, {"i", "ί"}, {"o", "ο"}, {"p", "ρ"}, {"u", "υ"}, {"x", "χ"}
-            };
-            foreach (char character in textBox1.Text)
-            {
-                if (!Regex.IsMatch(character.ToString(), "[aylkvioptux0-9-]"))
-                {
-                    MessageBox.Show("There was no substituted letter for '" + character + "'.\nStop searching for the Greek and Coptic subcategory.", "Exception - Greek and Coptic", MessageBoxButtons.OK, MessageBoxIcon.Stop);
-                    domain.Clear();
-                    break;
-                }
-                foreach (KeyValuePair<string, string> greek in GreekAndCopticTable)
-                {
-                    if (Regex.IsMatch(character.ToString(), greek.Key))
-                    {
-                        domain.Append(greek.Value);
-                        break;
-                    }
-                }
-                if (Regex.IsMatch(character.ToString(), "[0-9-]"))
-                {
-                    domain.Append(character);
-                }
-            }
-            textBox4.Text = domain.ToString();
-            if (!string.IsNullOrEmpty(domain.ToString()))
-            {
-                textBox3.Text = "http://" + domain.ToString() + ".com/";
-            }
+            textBox2.Text = SearchHomograph(CyrillicTable, "Cyrillic", CyrillicRegex);
             domain.Clear();
         }
 
-        private void button5_Click(object sender, EventArgs e) // Armenian
+        private void button2_Click(object sender, EventArgs e)
         {
-            var ArmenianTable = new Dictionary<string, string>()
-            {
-                {"t", "ե"}, {"q", "զ"}, {"d", "ժ"}, {"l", "լ"}, {"h", "հ"}, {"j", "յ"}, {"n", "ո"}, {"u", "ս"}, {"g", "ց"}, {"o", "օ"}
-            };
+            textBox4.Text = SearchHomograph(GreekAndCopticTable, "Greek and Coptic", GreekAndCopticRegex);
+            domain.Clear();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox5.Text = SearchHomograph(ArmenianTable, "Armenian", ArmenianRegex);
+            domain.Clear();
+        }
+
+        private string SearchHomograph(Dictionary<string, string> SubstitutedTable, string subcategory, string TableRegex)
+        {
             foreach (char character in textBox1.Text)
             {
-                if (!Regex.IsMatch(character.ToString(), "[tqdlhjnugo0-9-]"))
+                if (!Regex.IsMatch(character.ToString(), TableRegex))
                 {
-                    MessageBox.Show("There was no substituted letter for '" + character + "'.\nStop searching for the Armenian subcategory.", "Exception - Armenian", MessageBoxButtons.OK, MessageBoxIcon.Stop);
-                    domain.Clear();
-                    break;
+                    MessageBox.Show("There was no substituted letter for '" + character + "'.\nStop searching for the " + subcategory + " subcategory.", "Exception - " + subcategory, MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                    return String.Empty;
                 }
-                foreach (KeyValuePair<string, string> armenian in ArmenianTable)
+                foreach (KeyValuePair<string, string> SubChar in SubstitutedTable)
                 {
-                    if (Regex.IsMatch(character.ToString(), armenian.Key))
+                    if (Regex.IsMatch(character.ToString(), SubChar.Key))
                     {
-                        domain.Append(armenian.Value);
+                        domain.Append(SubChar.Value);
                         break;
                     }
                 }
@@ -161,12 +112,11 @@ namespace CyrillicHomographGen
                     domain.Append(character);
                 }
             }
-            textBox5.Text = domain.ToString();
             if (!string.IsNullOrEmpty(domain.ToString()))
             {
                 textBox3.Text = "http://" + domain.ToString() + ".com/";
             }
-            domain.Clear();
+            return domain.ToString();
         }
     }
 }
